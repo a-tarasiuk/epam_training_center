055174f864de5352f0db6e7e47c5c214e306ea89
”n199a” <n199a.info@gmail.com>
2021-12-06 13:35:52 +0300

    Removed unnecessary string fields and @JsonProperty annotations from ExceptionBody.class

--- module_4/gift_certificates_system/controller/src/main/java/com/epam/esm/controller/handler/ExceptionBody.java
+++ module_4/gift_certificates_system/controller/src/main/java/com/epam/esm/controller/handler/ExceptionBody.java
@@ -1,6 +1,5 @@
 package com.epam.esm.controller.handler;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,12 +12,6 @@ import java.util.Set;
 @Builder
 @ToString
 public class ExceptionBody {
-    private static final String ERROR_MESSAGE_PROPERTY = "Error message";
-    private static final String ERROR_CODE_PROPERTY = "Error esm code";
-
-    @JsonProperty(ERROR_MESSAGE_PROPERTY)
     private final Set<String> errorMessages;
-
-    @JsonProperty(ERROR_CODE_PROPERTY)
     private final int errorCode;
 }
