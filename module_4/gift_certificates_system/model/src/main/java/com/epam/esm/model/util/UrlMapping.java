be45a9462502ed1fbd50dd25761f60ef27ecbe0b
”n199a” <n199a.info@gmail.com>
2021-12-06 15:19:43 +0300

    Removed comments from UrlMapping.class

--- module_4/gift_certificates_system/model/src/main/java/com/epam/esm/model/util/UrlMapping.java
+++ module_4/gift_certificates_system/model/src/main/java/com/epam/esm/model/util/UrlMapping.java
@@ -8,44 +8,22 @@ import lombok.NoArgsConstructor;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class UrlMapping {
-
-    /**
-     * General URLs.
-     */
     public static final String ID = "/{id}";
     public static final String ALL_INSIDE = "/**";
 
-    /**
-     * URL for Gift certificate entities
-     */
     public static final String GIFT_CERTIFICATES = "/gift-certificates";
 
-    /**
-     * URL for Tag
-     */
     public static final String TAGS = "/tags";
     public static final String MOST_WIDELY_USED_TAG_OF_TOP_USER = "/most-widely-used-of-top-user";
 
-    /**
-     * URL only for User.
-     */
     public static final String USERS = "/users";
 
-    /**
-     * URL for User with Order
-     */
     public static final String FIND_ORDER_FOR_USER = "/users/{userId}/orders/{orderId}";
     public static final String ORDER_FOR_USER = "/users/{userId}/orders";
 
-    /**
-     * URL for Order
-     */
     public static final String ORDERS = "/orders";
     public static final String FIND_ORDERS_BY_ID = "/orders/{id}";
 
-    /**
-     * URL for Authentication
-     */
     public static final String AUTHENTICATION = "/auth";
     public static final String SIGN_UP = "/sign-up";
     public static final String SIGN_IN = "/sign-in";
