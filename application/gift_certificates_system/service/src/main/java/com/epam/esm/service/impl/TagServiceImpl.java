527732aa4532f4c9a83dafb9e0586e463e561742
Aliaksandr Tarasiuk <46199655+n199a@users.noreply.github.com>
2022-10-03 16:49:41 +0300

    Update TagServiceImpl.java

--- application/gift_certificates_system/service/src/main/java/com/epam/esm/service/impl/TagServiceImpl.java
+++ application/gift_certificates_system/service/src/main/java/com/epam/esm/service/impl/TagServiceImpl.java
@@ -1,24 +1,5 @@
 package com.epam.esm.service.impl;
 
-<<<<<<< HEAD
-import com.epam.esm.dao.TagDao;
-import com.epam.esm.entity.Tag;
-import com.epam.esm.exception.EntityExistsException;
-import com.epam.esm.exception.EntityInvalidException;
-import com.epam.esm.exception.EntityNotFoundException;
-import com.epam.esm.service.AbstractService;
-import com.epam.esm.util.TagValidator;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-import static com.epam.esm.util.PropertyKey.MESSAGE_ENTITY_EXISTS_EXCEPTION;
-import static com.epam.esm.util.PropertyKey.MESSAGE_ENTITY_INVALID_EXCEPTION;
-import static com.epam.esm.util.PropertyKey.MESSAGE_ENTITY_NOT_FOUND_EXCEPTION;
-=======
 import com.epam.esm.model.dto.TagDto;
 import com.epam.esm.model.dto.UserDto;
 import com.epam.esm.model.entity.GiftCertificate;
@@ -53,62 +34,10 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
->>>>>>> module_6
 
 /**
  * Tag service implementation.
  */
-<<<<<<< HEAD
-@Log4j2
-@Service
-public class TagServiceImpl implements AbstractService<Tag> {
-    private final TagDao tagDao;
-
-    /**
-     * Instantiates a new tag service.
-     *
-     * @param tagDao - Tag DAO layer.
-     */
-    @Autowired
-    public TagServiceImpl(TagDao tagDao) {
-        this.tagDao = tagDao;
-    }
-
-    @Override
-    public Tag create(Tag entity) {
-        if (!TagValidator.isValid(entity)) {
-            throw new EntityInvalidException(MESSAGE_ENTITY_INVALID_EXCEPTION);
-        }
-
-        String tagName = entity.getName();
-        Optional<Tag> optionalTag = tagDao.findByName(tagName);
-
-        if (optionalTag.isPresent()) {
-            throw new EntityExistsException(MESSAGE_ENTITY_EXISTS_EXCEPTION);
-        }
-
-        return tagDao.create(entity);
-    }
-
-    @Override
-    public List<Tag> findAll() {
-        return tagDao.findAll();
-    }
-
-    @Override
-    public Tag findById(long id) {
-        Optional<Tag> optionalTag = tagDao.findById(id);
-        return optionalTag.orElseThrow(() -> new EntityNotFoundException(MESSAGE_ENTITY_NOT_FOUND_EXCEPTION));
-    }
-
-    @Override
-    public boolean delete(long id) {
-        boolean result;
-        Optional<Tag> optionalTag = tagDao.findById(id);
-        optionalTag.orElseThrow(() -> new EntityNotFoundException(MESSAGE_ENTITY_NOT_FOUND_EXCEPTION));
-        result = tagDao.delete(id);
-        return result;
-=======
 @Service
 @Log4j2
 @Transactional
@@ -246,6 +175,5 @@ public class TagServiceImpl implements TagService<TagDto> {
         tagRepository.findByName(name).ifPresent(t -> {
             throw new EntityExistsException(MessagePropertyKey.EXCEPTION_TAG_NAME_EXISTS, name);
         });
->>>>>>> module_6
     }
 }
